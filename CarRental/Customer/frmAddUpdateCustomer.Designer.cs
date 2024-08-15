ebf58a7490f9ee2c3e520166df65d2a8c6aa3501
AbuBasssam <166512044+AbuBasssam@users.noreply.github.com>
2024-08-15 21:39:11 +0300

    Add files via upload

---
+++ CarRental/Customer/frmAddUpdateCustomer.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace CarRental
+{
+    partial class frmAddUpdateCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddUpdateCustomer));
+            this.lblTitle = new Guna.UI.WinForms.GunaLabel();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.ctrlPersonInfo1 = new CarRental.ctrlPersonInfo();
+            this.gpCustomerInfo = new System.Windows.Forms.GroupBox();
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.txtLicenseNo = new System.Windows.Forms.TextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.gpCustomerInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblTitle.Location = new System.Drawing.Point(436, 23);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(382, 46);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Add New Customer";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.White;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnClose.ForeColor = System.Drawing.Color.Navy;
+            this.btnClose.Image = global::CarRental.Properties.Resources.close;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.Location = new System.Drawing.Point(965, 541);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(126, 41);
+            this.btnClose.TabIndex = 147;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.White;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.Navy;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.Location = new System.Drawing.Point(1099, 541);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(126, 41);
+            this.btnSave.TabIndex = 146;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // ctrlPersonInfo1
+            // 
+            this.ctrlPersonInfo1.BackColor = System.Drawing.Color.White;
+            this.ctrlPersonInfo1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ctrlPersonInfo1.BackgroundImage")));
+            this.ctrlPersonInfo1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ctrlPersonInfo1.Location = new System.Drawing.Point(12, 72);
+            this.ctrlPersonInfo1.Name = "ctrlPersonInfo1";
+            this.ctrlPersonInfo1.Size = new System.Drawing.Size(1167, 400);
+            this.ctrlPersonInfo1.TabIndex = 2;
+            // 
+            // gpCustomerInfo
+            // 
+            this.gpCustomerInfo.BackColor = System.Drawing.Color.Transparent;
+            this.gpCustomerInfo.Controls.Add(this.lblCustomerID);
+            this.gpCustomerInfo.Controls.Add(this.label11);
+            this.gpCustomerInfo.Controls.Add(this.pictureBox9);
+            this.gpCustomerInfo.Controls.Add(this.txtLicenseNo);
+            this.gpCustomerInfo.Controls.Add(this.pictureBox5);
+            this.gpCustomerInfo.Controls.Add(this.label6);
+            this.gpCustomerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.gpCustomerInfo.ForeColor = System.Drawing.Color.White;
+            this.gpCustomerInfo.Location = new System.Drawing.Point(12, 487);
+            this.gpCustomerInfo.Name = "gpCustomerInfo";
+            this.gpCustomerInfo.Size = new System.Drawing.Size(834, 86);
+            this.gpCustomerInfo.TabIndex = 158;
+            this.gpCustomerInfo.TabStop = false;
+            this.gpCustomerInfo.Text = "Customer Info";
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblCustomerID.Location = new System.Drawing.Point(187, 36);
+            this.lblCustomerID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(48, 25);
+            this.lblCustomerID.TabIndex = 162;
+            this.lblCustomerID.Text = "???";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Navy;
+            this.label11.Location = new System.Drawing.Point(7, 36);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(133, 25);
+            this.label11.TabIndex = 161;
+            this.label11.Text = "CustomerID:";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox9.Image = global::CarRental.Properties.Resources.Number_32;
+            this.pictureBox9.Location = new System.Drawing.Point(147, 36);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(31, 26);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 163;
+            this.pictureBox9.TabStop = false;
+            // 
+            // txtLicenseNo
+            // 
+            this.txtLicenseNo.Location = new System.Drawing.Point(421, 33);
+            this.txtLicenseNo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLicenseNo.MaxLength = 50;
+            this.txtLicenseNo.Name = "txtLicenseNo";
+            this.txtLicenseNo.Size = new System.Drawing.Size(167, 30);
+            this.txtLicenseNo.TabIndex = 160;
+            this.txtLicenseNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLicenseNo_KeyPress);
+            this.txtLicenseNo.Validating += new System.ComponentModel.CancelEventHandler(this.txtLicenseNo_Validating_1);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(383, 36);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(31, 26);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 159;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Navy;
+            this.label6.Location = new System.Drawing.Point(243, 36);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 25);
+            this.label6.TabIndex = 158;
+            this.label6.Text = "License No :";
+            // 
+            // frmAddUpdateCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(1238, 596);
+            this.Controls.Add(this.gpCustomerInfo);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.ctrlPersonInfo1);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmAddUpdateCustomer";
+            this.Text = "frmAddUpdateCustomer";
+            this.Load += new System.EventHandler(this.frmAddUpdateCustomer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.gpCustomerInfo.ResumeLayout(false);
+            this.gpCustomerInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaLabel lblTitle;
+        private ctrlPersonInfo ctrlPersonInfo1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox gpCustomerInfo;
+        private System.Windows.Forms.Label lblCustomerID;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.TextBox txtLicenseNo;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ CarRental/Customer/frmCustomerInfo.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CarRental
+{
+    partial class frmCustomerInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCustomerInfo));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.ctrlCustomerCard1 = new CarRental.ctrlCustomerCard();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Image = global::CarRental.Properties.Resources.close;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.Location = new System.Drawing.Point(714, 409);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(125, 50);
+            this.btnClose.TabIndex = 19;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ctrlCustomerCard1
+            // 
+            this.ctrlCustomerCard1.BackColor = System.Drawing.Color.Transparent;
+            this.ctrlCustomerCard1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ctrlCustomerCard1.BackgroundImage")));
+            this.ctrlCustomerCard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ctrlCustomerCard1.Location = new System.Drawing.Point(-1, -6);
+            this.ctrlCustomerCard1.Name = "ctrlCustomerCard1";
+            this.ctrlCustomerCard1.Size = new System.Drawing.Size(840, 410);
+            this.ctrlCustomerCard1.TabIndex = 20;
+            // 
+            // frmCustomerInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(848, 465);
+            this.Controls.Add(this.ctrlCustomerCard1);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmCustomerInfo";
+            this.Text = "frmCustomerInfo";
+            this.Load += new System.EventHandler(this.frmCustomerInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClose;
+        private ctrlCustomerCard ctrlCustomerCard1;
+    }
+}
