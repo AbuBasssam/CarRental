450692f2d1e54502a6e4d8743b79d6337a63ab79
AbuBasssam <166512044+AbuBasssam@users.noreply.github.com>
2024-08-15 21:45:49 +0300

    Add files via upload

---
+++ CarRental/stringBuilderLabel.cs
@@ -0,0 +1,61 @@
+﻿using Guna.UI.WinForms;
+using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental
+{
+    public partial class MyLabel : GunaLabel
+    {
+        public MyLabel()
+        {
+            InitializeComponent();
+        }
+
+        public MyLabel(IContainer container)
+        {
+            container.Add(this);
+
+
+            InitializeComponent();
+           
+
+        }
+        private StringBuilder _textBuilder=new StringBuilder();
+
+        
+
+        public StringBuilder TextBuilder
+        {
+            get => _textBuilder;
+            set
+            {
+                if (_textBuilder != value)
+                {
+                    _textBuilder = value;
+                    OnTextChanged(EventArgs.Empty);
+                }
+            }
+        }
+
+        public override string Text
+        {
+            get => _textBuilder.ToString();
+            set
+            {
+                if (_textBuilder.ToString() != value)
+                {
+                    _textBuilder.Clear();
+                    _textBuilder.Append(value);
+                    OnTextChanged(EventArgs.Empty);
+                }
+            }
+        }
+
+    }
+}
